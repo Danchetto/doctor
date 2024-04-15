71f27a1a5af82957fc6f45f029e81b9d9191f492
Daniil Tuchin <danchetto@yandex.ru>
2024-04-15 21:02:47 +0300

    updates

--- src/components/doctors/index.tsx
+++ src/components/doctors/index.tsx
@@ -56,6 +56,7 @@ const Doctors: React.FC = () => {
             Petr Tuchin - Doctoralia.es
           </a>
           <Script
+            id="doc"
             dangerouslySetInnerHTML={{ __html: '!function($_x,_s,id){var js,fjs=$_x.getElementsByTagName(_s)[0];if(!$_x.getElementById(id)){js = $_x.createElement(_s);js.id = id;js.src = "//platform.docplanner.com/js/widget.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","zl-widget-s");' }}
           />
         </div>
